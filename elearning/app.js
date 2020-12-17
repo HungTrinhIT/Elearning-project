901eff35896fe938755ebcf0abfed7cd51a5bcd4
HungTrinh99 <minhhung.it99@gmail.com>
2020-12-17 18:31:40 +0700

    add: new template online academy

--- elearning/app.js
+++ elearning/app.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 var exphbs = require("express-handlebars");
-// To use static file in public folder
 app.use(express.static(__dirname + "/public"));
 
-//Declare application use handlebar view engine
 app.engine(
   "hbs",
   exphbs({
     defaultLayout: "main.hbs",
     extname: ".hbs",
-    _partialsDir: "views/_partials",
-    _layoutsDir: "views/_layouts",
+    partialsDir: "views/_partials",
+    layoutsDir: "views/_layouts",
   })
 );
 app.set("view engine", "hbs");
@@ -26,6 +24,8 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
+app.use("/admin/categories", require("./routes/category.route"));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
